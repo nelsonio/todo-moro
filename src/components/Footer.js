29446457180b00ccba272042cc8031401b61e674
Martin Haupt <martin23haupt@gmail.com>
2019-06-29 22:38:42 +0200

    mark all completed a delete all completed works

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import { toggleTodo, deleteTodo } from '../store/actions.js';
 
 class Footer extends Component {
+    deleteCompleted = () => {
+        this.props.todos
+            .filter(todo => todo.completed)
+            .forEach(todo => {
+                this.props.deleteTodo(todo.id);
+            });
+    };
+
+    markAllCompleted = () => {
+        this.props.todos
+            .filter(todo => !todo.completed)
+            .forEach(todo => {
+                this.props.toggleTodo(todo.id, false);
+            });
+    };
+
     render() {
         const { completed, left } = this.props.todos.reduce(
             (stats, todo) => {
@@ -20,7 +37,13 @@ class Footer extends Component {
         );
         return (
             <footer>
-                {completed} completed, {left} left
+                <p>
+                    {completed} completed, {left} left
+                </p>
+                <button onClick={this.deleteCompleted}>Clear comleted</button>
+                <button onClick={this.markAllCompleted}>
+                    Mark all completed
+                </button>
             </footer>
         );
     }
@@ -32,5 +55,5 @@ const mapStateToProps = state => ({
 
 export default connect(
     mapStateToProps,
-    {}
+    { toggleTodo, deleteTodo }
 )(Footer);
