a1d370b14b677ad879d78ce3638a071027545062
Martin Haupt <martin23haupt@gmail.com>
2019-07-01 08:47:16 +0200

    final (?) small corrections

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -4,23 +4,13 @@ import PropTypes from 'prop-types';
 import './Icons.css';
 
 export const Pen = () => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1"
-        viewBox="0 0 24 24"
-        className="icon pen"
-    >
+    <svg version="1" viewBox="0 0 24 24" className="icon pen">
         <path d="M24 5V4l-4-4h-1l-3 3 5 5M0 19v5h5L20 9l-5-5L0 19z" />
     </svg>
 );
 
 export const Remove = () => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        version="1"
-        viewBox="0 0 24 24"
-        className="icon remove"
-    >
+    <svg version="1" viewBox="0 0 24 24" className="icon remove">
         <path d="M12 0a12 12 0 1 1 0 24 12 12 0 0 1 0-24m4 6l-4 4-4-4-2 2 4 4-4 4 2 2 4-4 4 4 2-2-4-4 4-4-2-2z" />
     </svg>
 );
